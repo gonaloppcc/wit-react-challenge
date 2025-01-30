8c9a0bd263bad0eeb5ff9ddd5d9ca215f857ac63
Gonçalo Soares <goncalosoares555@gmail.com>
2025-01-30 02:42:19 +0000

    fix: remove unused import to fix eslint error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type {Metadata} from "next";
 import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
-import {isServer, QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import React from "react";
 import {Providers} from "@/components/Providers";
 

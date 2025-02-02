82c5666103ec435e82347a3de1b7c1c9250c6b2b
Gonçalo Soares <goncalosoares555@gmail.com>
2025-02-02 21:28:31 +0000

    feat: add getWeatherForecastByCityName and getWeatherForecastByCoordinates function. refactor of other weather api functions

--- src/services/weather.ts
+++ src/services/weather.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import {Weather} from "@/model/Weather";
 
 /*
     standard: Kelvin
@@ -52,6 +53,10 @@ export interface WeatherData {
     cod: number
 }
 
+interface WeatherForecastData {
+    list: WeatherData[];
+}
+
 export interface Coordinates {
     name: string;
     local_names: {
@@ -64,7 +69,7 @@ export interface Coordinates {
 }
 
 const BASE_URL = 'https://api.openweathermap.org/';
-export const getWeatherByCityName = async (cityName: string, units: WeatherUnits = 'standard') => {
+export const getCurrentWeatherByCityName = async (cityName: string, units: WeatherUnits = 'standard') => {
     const coordinates = await getCoordinatesByCityName(cityName);
 
     // If the returned array is empty, it means the city was not found
@@ -73,7 +78,24 @@ export const getWeatherByCityName = async (cityName: string, units: WeatherUnits
     }
 
     const {lat, lon} = coordinates[0];
-    return await getWeatherByCoordinates(lat, lon, units);
+    return await getCurrentWeatherByCoordinates(lat, lon, units);
+}
+
+export const getWeatherForecastByCityName = async (
+    cityName: string,
+    units: WeatherUnits = 'standard',
+    cnt: number = 40,
+    lang: string = 'en'
+) => {
+    const coordinates = await getCoordinatesByCityName(cityName);
+
+    // If the returned array is empty, it means the city was not found
+    if (coordinates.length === 0) {
+        throw new Error('City not found');
+    }
+
+    const {lat, lon} = coordinates[0];
+    return await getWeatherForecastByCoordinates(lat, lon, cnt, units, lang);
 }
 
 const getCoordinatesByCityName = async (cityName: string, limit: number = 1) => {
@@ -89,11 +111,11 @@ const getCoordinatesByCityName = async (cityName: string, limit: number = 1) => 
     })).data as Coordinates[];
 }
 
-const getWeatherByCoordinates = async (lat: number, lon: number, units: WeatherUnits = 'standard') => {
+const getCurrentWeatherByCoordinates = async (lat: number, lon: number, units: WeatherUnits = 'standard') => {
     const PATH = `data/2.5/weather`;
     const URL = `${BASE_URL}${PATH}`;
 
-    return (await axios.get(URL, {
+    const weatherData = (await axios.get(URL, {
         params: {
             lat,
             lon,
@@ -102,4 +124,58 @@ const getWeatherByCoordinates = async (lat: number, lon: number, units: WeatherU
             appid: process.env.NEXT_PUBLIC_OPENWEATHERMAP_API_KEY
         }
     })).data as WeatherData;
+
+    const weatherModelData: Weather = {
+        dt: weatherData.dt,
+        temp: weatherData.main.temp,
+        feels_like: weatherData.main.feels_like,
+        temp_min: weatherData.main.temp_min,
+        temp_max: weatherData.main.temp_max,
+        pressure: weatherData.main.pressure,
+        humidity: weatherData.main.humidity,
+        sea_level: weatherData.main.sea_level,
+        grnd_level: weatherData.main.grnd_level,
+        weather: weatherData.weather,
+    }
+
+    return weatherModelData;
+}
+
+const getWeatherForecastByCoordinates = async (
+    lat: number,
+    lon: number,
+    cnt: number = 40,
+    units: WeatherUnits = 'standard',
+    lang: string = 'en'
+) => {
+    const PATH = `data/2.5/forecast`;
+    const URL = `${BASE_URL}${PATH}`;
+
+    const forecastData = (await axios.get(URL, {
+        params: {
+            lat,
+            lon,
+            cnt,
+            units,
+            lang,
+            appid: process.env.NEXT_PUBLIC_OPENWEATHERMAP_API_KEY
+        }
+    })).data as WeatherForecastData;
+
+    const weatherModelData: Weather[] = forecastData.list.map(data => {
+        return {
+            dt: data.dt,
+            temp: data.main.temp,
+            feels_like: data.main.feels_like,
+            temp_min: data.main.temp_min,
+            temp_max: data.main.temp_max,
+            pressure: data.main.pressure,
+            humidity: data.main.humidity,
+            sea_level: data.main.sea_level,
+            grnd_level: data.main.grnd_level,
+            weather: data.weather,
+        }
+    })
+
+    return weatherModelData;
 }
